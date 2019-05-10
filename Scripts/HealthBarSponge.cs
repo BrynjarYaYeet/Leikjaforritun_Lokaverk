502c01cd1c8315c3a18cb8c3221a6e425a3b995d
SigursteinnOli <34093550+SigursteinnOli@users.noreply.github.com>
2019-05-10 11:26:40 +0000

    Update HealthBarSponge.cs

--- Scripts/HealthBarSponge.cs
+++ Scripts/HealthBarSponge.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Þetta script heldur utan um Health hjá "Svamp Sveinsson" 
+// Bara basic float breyta
+// Síðan tengjum við scriptuna við Healthbar animationið til þess að sýna fyrir spilaranum að hann er að tapa health
+// (Healthbarinn er bara animation)
+
+
 public class HealthBarSponge : MonoBehaviour
 {
 
