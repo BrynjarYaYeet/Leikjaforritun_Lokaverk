9fdfca31985a5df77d577c32b9c5e14935066ca6
SigursteinnOli <34093550+SigursteinnOli@users.noreply.github.com>
2019-05-10 11:34:36 +0000

    Update hit_ninja.cs

--- Scripts/hit_ninja.cs
+++ Scripts/hit_ninja.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// HitNinja er script til þess að finna út hvort að Ninjan hitti óvininn þegar hann lemur
+// og til þess að taka health af óvininum ef það gerist
+
+
+
 public class hit_ninja : MonoBehaviour
 {
     public float Health;
@@ -10,19 +15,24 @@ public class hit_ninja : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Hér tengjum við okkur við healthbar scriptuna hjá SpongeBob
         GameObject HealthBar = GameObject.Find("HealthBarSponge");
         HealthBarSponge HBS = HealthBar.GetComponent<HealthBarSponge>();
         Health = 100;
         Svamp.SetActive(false);
     }
 
+    //Við settum collider á sverðið inni í animationinu þannig colliderinn er bara active þegar þú lemur
     void OnTriggerEnter2D(Collider2D collision)
     {
+        //Checkum hvort það sem þú lamdir var óvinurinn
         if (collision.gameObject.name.Equals("Player2"))
         {
+            //í hvert skipti sem þú lemur hann tökum við health af og referencum Healthbar scriptuna
             Health -= 10;
             HBS.cur_Health = Health;
 
+            // Og að sjálfssögðu þegar health er 0 þá "deyr" óvinurinn
             if (Health <= 0f)
             {
                 collision.gameObject.SetActive(false);
